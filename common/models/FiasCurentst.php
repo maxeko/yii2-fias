ab0f8bc11a03eb79a9a3bd9e9780ee071e5fe22a
maxek <korshunov.m.e@gmail.com>
2016-07-27 12:49:57 +0500

    getDb in models

--- common/models/FiasCurentst.php
+++ common/models/FiasCurentst.php
@@ -4,6 +4,11 @@ namespace ejen\fias\common\models;
 
 class FiasCurentst extends \yii\db\ActiveRecord
 {
+    public static function getDb()
+    {
+        return \ejen\fias\Module::getInstance()->getDb();
+    }
+
     public static function tableName()
     {
         return '{{%fias_curentst}}';
--- common/models/FiasHouseint.php
+++ common/models/FiasHouseint.php
@@ -8,7 +8,7 @@ class FiasHouseint extends \yii\db\ActiveRecord
     {
         return \ejen\fias\Module::getInstance()->getDb();
     }
-    
+
     public static function tableName()
     {
         return '{{%fias_houseint}}';
