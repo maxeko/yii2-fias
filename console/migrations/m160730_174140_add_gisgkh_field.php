4294836655dc83bf7c5cbda25d26774db5c8a96d
Vladislav Shumilov <vladislav.shumilov@gmail.com>
2016-07-31 04:13:41 +0500

    Feature #222

---
+++ console/migrations/m160730_174140_add_gisgkh_field.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+class m160730_174140_add_gisgkh_field extends Migration
+{
+
+    public $gisGkhField = 'gisgkh';
+    public $module;
+
+    public function init()
+    {
+        $this->module = ejen\fias\Module::getInstance();
+
+        if (!empty($this->module)) {
+            $this->db = $this->module->getDb();
+        }
+
+        parent::init();
+    }
+
+    public function up()
+    {
+        $this->addColumn($this->module->fiasAddrobjTable, $this->gisGkhField, $this->boolean()->notNull()->defaultValue(false));
+        
+        $this->addColumn($this->module->fiasHouseTable, $this->gisGkhField, $this->boolean()->notNull()->defaultValue(false));
+    }
+
+    public function down()
+    {
+        $this->dropColumn($this->module->fiasAddrobjTable, $this->gisGkhField);
+        
+        $this->dropColumn($this->module->fiasHouseTable, $this->gisGkhField);
+    }
+}
