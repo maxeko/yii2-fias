7fe6ad2e57652100ceb5e59403aae31a58d3a49a
korshunovmaxim <korshunov.m.e@gmail.com>
2016-08-01 09:20:59 +0500

    console commands fias/migrate and fias/migrate/down

---
+++ console/controllers/MigrateController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ejen\fias\console\controllers;
+
+use yii\console\Controller;
+use ejen\fias\Module;
+
+class MigrateController extends Controller
+{
+    public function actionIndex()
+    {
+        exec(
+            'php ' . \Yii::$app->basePath . '/yii migrate' .
+            ' --interactive=0' .
+            ' --migrationPath=' . Module::getInstance()->basePath . '/console/migrations' .
+            ' --db=' . Module::getInstance()->db,
+            $output
+        );
+
+        array_walk($output, function ($line) {
+            echo sprintf("%s\n", $line);
+        });
+    }
+
+    public function actionDown()
+    {
+        exec(
+            'php ' . \Yii::$app->basePath . '/yii migrate/down' .
+            ' --interactive=0' .
+            ' --migrationPath=' . Module::getInstance()->basePath . '/console/migrations' .
+            ' --db=' . Module::getInstance()->db . ' 19',
+            $output
+        );
+
+        array_walk($output, function ($line) {
+            echo sprintf("%s\n", $line);
+        });
+    }
+}
