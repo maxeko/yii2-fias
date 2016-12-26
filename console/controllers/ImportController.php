43aa679338acd1f21c77d640ced0a421d2b53d70
Viktor Dakalov <atlantidu@gmail.com>
2016-12-26 17:21:18 +0500

    Update new versions for fias

--- console/controllers/ImportController.php
+++ console/controllers/ImportController.php
@@ -25,11 +25,16 @@ use ejen\fias\common\models\FiasSocrbase;
 use ejen\fias\Module;
 use yii\console\Controller;
 
-class FiasController extends Controller
+class ImportController extends Controller
 {
     public $region;
 
     public $versions;
+
+    public $type = 0; // тип импорта 1 - ФИАС, 2 - ГИС
+
+    const TYPE_FIAS = 1;
+    const TYPE_GIS = 2;
 
     public function options($actionID)
     {
@@ -41,10 +46,176 @@ class FiasController extends Controller
     private function loadVersions()
     {
         $client = new \SoapClient('http://fias.nalog.ru/WebServices/Public/DownloadService.asmx?WSDL');
-        $this->versions = $client->GetAllDownloadFileInfo();
+        $result = $client->GetAllDownloadFileInfo();
+
+        if ($result) {
+            $this->versions = $result->GetAllDownloadFileInfoResult->DownloadFileInfo;
+            $this->stdout("Версий загружено: " . (count($this->versions)) . "\n");
+        }
     }
 
-    public function actionImportDbfFolder($path)
+    private function getVersionFromDate(\DateTime $date)
+    {
+        if (!$this->versions) {
+            $this->stdout("Загрузка версий ФИАС\n");
+            $this->loadVersions();
+        }
+
+        $list = [];
+
+        foreach ($this->versions as $version) {
+            $versionDate = new \DateTime(substr($version->TextVersion, -10));
+            if ($versionDate > $date) {
+                $list[] = $version;
+            }
+        }
+
+        return $list;
+    }
+
+    public function actionUpdateFias()
+    {
+        $this->type = ImportController::TYPE_FIAS;
+
+        $db = Module::getInstance()->getDb();
+
+        $result = $db->createCommand("SELECT * FROM system WHERE type = 'FIAS' ORDER BY date ASC LIMIT 1")->query();
+        $rows = $result->readAll();
+
+        if (!count($rows)) {
+            $this->stdout("Текущая версия базы не известна. Необходимо внести запись в таблицу \"system\"\n");
+            return 0;
+        }
+
+        $versions = $this->getVersionFromDate(new \DateTime($rows[0]['version']));
+        $len = count($versions);
+
+        if (!$len) {
+            $this->stdout("Новых версий не найдено\n");
+            return 0;
+        }
+
+        $this->stdout("Новых версий: {$len}\n");
+
+        foreach ($versions as $version) {
+            $this->stdout("Импорт версии #{$version->VersionId} \"{$version->TextVersion}\" ($version->FiasDeltaDbfUrl)\n");
+            $this->actionFromUrl($version->FiasDeltaDbfUrl);
+        }
+
+        return 0;
+    }
+
+    public function actionUpdateGis($path)
+    {
+        $this->type = ImportController::TYPE_GIS;
+
+        $this->actionFromZip($path);
+    }
+
+    public function actionFromUrl($url)
+    {
+        $filename = basename($url);
+        $path = "/tmp/{$filename}";
+
+        $headers = get_headers($url, 1);
+
+        if ($headers[0] != "HTTP/1.1 200 OK") {
+            $this->stderr("Ошибка загрузки файла (" . $headers[0] . ")\n");
+            return 1;
+        }
+
+        $length = (int)$headers['Content-Length'];
+        $data = "";
+        $loaded = 0;
+
+        $fp = fopen($url, 'rb');
+
+        if (!$fp) {
+            $this->stderr("Ошибка загрузки файла\n");
+            return 1;
+        }
+
+        $dfp = fopen($path, "wb");
+
+        if (!$dfp) {
+            $this->stderr("Ошибка создания локального файла");
+            return 1;
+        }
+
+        $this->stdout("Загрузка файла...");
+
+        while (!feof($fp)) {
+            $part = fread($fp, 1000);
+            $data .= $part;
+            $loaded += strlen($part);
+
+            if (strlen($data) >= 2.684e+8 || feof($fp)) {
+                fwrite($dfp, $data, strlen($data));
+                $data = "";
+            }
+
+            $this->stdout("\rЗагружено " . round($loaded / ($length / 100), 2) . "%");
+        }
+
+        fclose($fp);
+        fclose($dfp);
+
+        $this->stdout("\rЗагружено " . round(($loaded / 1048576), 2) . " Мб\n");
+
+        $this->actionFromRar($path);
+
+        return 0;
+    }
+
+    public function actionFromRar($filename)
+    {
+        $destination = "{$filename}_extracted";
+        $command = "unrar e -y {$filename} *.DBF $destination/";
+
+        $this->stdout("Извлечение файлов в {$destination}\n");
+
+        exec($command, $output, $return);
+
+        if ($return) {
+            $this->stderr("Ошибка извлечения\n");
+            $this->stderr("{$command}\n");
+            $this->stderr(join("\n", $output));
+            return 1;
+        }
+
+        $this->actionFromFolder($destination);
+
+        return 0;
+    }
+
+    public function actionFromZip($filename)
+    {
+        $destination = "{$filename}_extracted";
+        $zip = new \ZipArchive();
+        $status = $zip->open($filename, \ZipArchive::EXCL);
+
+        if ($status !== true) {
+            $this->stderr("Ошибка открытия ZIP архива\n");
+            $this->stderr($zip);
+            return 1;
+        }
+
+        $this->stdout("Извлечение файлов в {$destination}");
+        $status = $zip->extractTo($destination);
+
+        if (!$status) {
+            $this->stderr('Ошибка извлечения файлов');
+            return 1;
+        }
+
+        $zip->close();
+
+        $this->actionFromFolder($destination);
+
+        return 0;
+    }
+
+    public function actionFromFolder($path)
     {
         $path = realpath($path);
 
@@ -80,51 +251,47 @@ class FiasController extends Controller
         return 0;
     }
 
-    public function actionImportDbfFile($filename)
+    public function actionFromDbfFile($filename)
     {
         $db = @dbase_open($filename, 0);
 
-        if (!$db)
-        {
+        if (!$db) {
             $this->stderr("Не удалось открыть DBF файл: '$filename'\n");
             return 1;
         }
 
         $classMap = [
-            '/^.*DADDROBJ\.DBF$/'   => FiasDaddrobj::className(),
-            '/^.*ADDROBJ\.DBF$/'    => FiasAddrobj::className(),
-            '/^.*LANDMARK\.DBF$/'   => FiasLandmark::className(),
-            '/^.*DHOUSE\.DBF$/'     => FiasDhouse::className(),
-            '/^.*HOUSE\d\d\.DBF$/'  => FiasHouse::className(),
-            '/^.*DHOUSINT\.DBF$/'   => FiasDhousint::className(),
-            '/^.*HOUSEINT\.DBF$/'   => FiasHouseint::className(),
-            '/^.*DLANDMRK\.DBF$/'   => FiasDlandmrk::className(),
-            '/^.*DNORDOC\.DBF$/'    => FiasDnordoc::className(),
+            '/^.*DADDROBJ\.DBF$/' => FiasDaddrobj::className(),
+            '/^.*ADDROBJ\.DBF$/' => FiasAddrobj::className(),
+            '/^.*LANDMARK\.DBF$/' => FiasLandmark::className(),
+            '/^.*DHOUSE\.DBF$/' => FiasDhouse::className(),
+            '/^.*HOUSE\d\d\.DBF$/' => FiasHouse::className(),
+            '/^.*DHOUSINT\.DBF$/' => FiasDhousint::className(),
+            '/^.*HOUSEINT\.DBF$/' => FiasHouseint::className(),
+            '/^.*DLANDMRK\.DBF$/' => FiasDlandmrk::className(),
+            '/^.*DNORDOC\.DBF$/' => FiasDnordoc::className(),
             '/^.*NORDOC\d\d\.DBF$/' => FiasNordoc::className(),
-            '/^.*ACTSTAT\.DBF$/'    => FiasActstat::className(),
-            '/^.*CENTERST\.DBF$/'   => FiasCenterst::className(),
-            '/^.*ESTSTAT\.DBF$/'    => FiasEststat::className(),
-            '/^.*HSTSTAT\.DBF$/'    => FiasHststat::className(),
-            '/^.*OPERSTAT\.DBF$/'   => FiasOperstat::className(),
-            '/^.*INTVSTAT\.DBF$/'   => FiasIntvstat::className(),
-            '/^.*STRSTAT\.DBF$/'    => FiasStrstat::className(),
-            '/^.*CURENTST\.DBF$/'   => FiasCurentst::className(),
-            '/^.*SOCRBASE\.DBF$/'   => FiasSocrbase::className(),
+            '/^.*ACTSTAT\.DBF$/' => FiasActstat::className(),
+            '/^.*CENTERST\.DBF$/' => FiasCenterst::className(),
+            '/^.*ESTSTAT\.DBF$/' => FiasEststat::className(),
+            '/^.*HSTSTAT\.DBF$/' => FiasHststat::className(),
+            '/^.*OPERSTAT\.DBF$/' => FiasOperstat::className(),
+            '/^.*INTVSTAT\.DBF$/' => FiasIntvstat::className(),
+            '/^.*STRSTAT\.DBF$/' => FiasStrstat::className(),
+            '/^.*CURENTST\.DBF$/' => FiasCurentst::className(),
+            '/^.*SOCRBASE\.DBF$/' => FiasSocrbase::className(),
         ];
 
         /* @var \yii\db\ActiveRecord $model */
         $modelClass = null;
-        foreach($classMap as $pattern => $className)
-        {
-            if (preg_match($pattern, $filename))
-            {
+        foreach ($classMap as $pattern => $className) {
+            if (preg_match($pattern, $filename)) {
                 $modelClass = $className;
                 break;
             }
         }
 
-        if ($modelClass === null)
-        {
+        if ($modelClass === null) {
             $this->stderr("Не поддерживаемый DBF файл: '$filename'\n");
             return 1;
         }
@@ -142,8 +309,7 @@ class FiasController extends Controller
 
         $primaries = $model->getPrimaryKey(true);
 
-        if (!count($primaries))
-        {
+        if (!count($primaries)) {
             $this->stdout("В структуре таблицы \"{$modelClass::tableName()}\" не определены первичные ключи\n");
             return 0;
         }
@@ -153,30 +319,25 @@ class FiasController extends Controller
         $deleted = 0;
         $inserted = 0;
 
-        for ($i = 0; $i < $rowsCount; $i++)
-        {
+        for ($i = 0; $i < $rowsCount; $i++) {
             $row = dbase_get_record_with_names($db, $i + 1);
 
             $insertRow = [];
 
-            foreach ($row as $column => $value)
-            {
-                if ($column == 'delete')
-                {
+            foreach ($row as $column => $value) {
+                if ($column == 'delete') {
                     continue;
                 }
 
                 $column = strtolower($column);
 
-                if (!$model->hasAttribute($column))
-                {
+                if (!$model->hasAttribute($column)) {
                     continue;
                 }
 
                 $value = mb_convert_encoding($value, 'UTF-8', 'CP866');
 
-                if ($i == 0)
-                {
+                if ($i == 0) {
                     $columns[] = strtolower($column);
                 }
 
@@ -190,8 +351,8 @@ class FiasController extends Controller
             $insertRows[] = $insertRow;
 
             if (($i && $i % $batchSize == 0) || $i == $rowsCount - 1) {
-                $deleted+= $model->getDb()->createCommand()->delete($modelClass::tableName(), $primariesValues)->execute();
-                $inserted+= $model->getDb()->createCommand()->batchInsert($modelClass::tableName(), $columns, $insertRows)->execute();
+                $deleted += $model->getDb()->createCommand()->delete($modelClass::tableName(), $primariesValues)->execute();
+                $inserted += $model->getDb()->createCommand()->batchInsert($modelClass::tableName(), $columns, $insertRows)->execute();
                 $primariesValues = [];
                 $insertRows = [];
                 $this->stdout("Обработано " . ($i + 1) . " из $rowsCount записей\r");
@@ -201,6 +362,11 @@ class FiasController extends Controller
         $this->stdout("\nОбновлено $deleted, добавлено " . ($inserted - $deleted) . "\n");
 
         return 0;
+    }
+
+    public function actionFromCsvFile($filename)
+    {
+
     }
 
     public function actionImportDbf($filename)
@@ -302,7 +468,7 @@ class FiasController extends Controller
                 $this->stdout("Обработано $i из $rowsCount записей\n");
             }
         }
-        
+
         if (!empty($insertRows)) {
             $transaction = Module::getInstance()->getDb()->beginTransaction();
             Module::getInstance()->getDb()->createCommand()->batchInsert($modelClass::tableName(), $columns, $insertRows)->execute();
